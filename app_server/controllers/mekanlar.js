e6f6d10336a19fdc17a89e671e5387b52b45f1c0
Deniz YILDIZ <denizyildiz000@outlook.com>
2022-12-04 20:49:05 +0300

    ödev7

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -13,13 +13,13 @@ var mesafeyiFormatla = function(mesafe) {
         birim="km";
     }else {
         yeniMesafe= parseInt(mesafe*1000,10);
-        birim = "m"
+        birim = "m";
     }
 return yeniMesafe+birim;
 }
 var anaSayfaOlustur=function(res,mekanListesi) {
     var mesaj;
-    if(!(mekanListesi instanceof(Array))){
+    if(!(mekanListesi instanceof Array)){
         mesaj="API HATASI: Bir şeyler ters gitti.";
         mekanListesi=[];
     } else {
@@ -28,22 +28,22 @@ var anaSayfaOlustur=function(res,mekanListesi) {
         }
     }
     res.render("anasayfa",{
-        "baslik":"Anasayfa",
-        "sayfaBaslik": {
-            "siteAd":"MekanBul",
-            "slogan":"Mekanları Keşfet"
+        baslik: "Anasayfa",
+        sayfaBaslik: {
+            siteAd:"MekanBul",
+            slogan:"Mekanları Keşfet"
         },
-        "mekanlar": mekanListesi,
-        "mesaj": mesaj
+        mekanlar: mekanListesi,
+        mesaj: mesaj
     });
 }
-const anaSayfa=function(req, res, next) {
+const anaSayfa=function(req, res) {
     axios.get(apiSecenekleri.sunucu+apiSecenekleri.apiYolu, {
         params:{
             enlem:req.query.enlem,
             boylam:req.query.boylam
         }
-    }).then(function(response){
+    }).then(function (response){
         var i,mekanlar;
         mekanlar = response.data;
         for(i=0;i<mekanlar.length;i++){
@@ -54,46 +54,41 @@ const anaSayfa=function(req, res, next) {
         anaSayfaOlustur(res,hata);
     });
 }
-
+var detaySayfasiOlustur = function (res, mekanDetaylari) {
+    mekanDetaylari.koordinat = {
+        "enlem": mekanDetaylari.koordinat[0],
+        "boylam":mekanDetaylari.koordinat[1]
+    }
+    res.render('mekanbilgisi',
+    {
+        mekanBaslik: mekanDetaylari.ad,
+        mekanDetay: mekanDetaylari
+    });
+}
+var hataGoster = function (res,hata) {
+    var mesaj;
+    if(hata.response.status==404) {
+        mesaj="404, Sayfa Bulunamadı!";
+    }
+    else{
+        mesaj=hata.response.status+" hatası";
+    }
+    res.status(hata.response.status);
+    res.render('error', {
+        "mesaj":mesaj
+    });
+}
 
 const mekanBilgisi=function(req, res) {
-    res.render('mekanbilgisi',
-    { 
-        "baslik":"Mekan Bilgisi",
-        "mekanBaslik":"Starbucks",
-        "mekanDetay": {
-            "ad":"Starbucks",
-            "adres":"Modernevler",
-            "puan":"4",
-            "imkanlar": ["Kahve","Çay","Kek"],
-            "koordinatlar": {
-                "enlem":"37.7",
-                "boylam":"30.5"
-            },
-            "saatler": [
-                {
-                    "gunler":"Pazartesi-Cuma",
-                    "acilis":"9:00",
-                    "kapanis":"23:00",
-                    "kapali": false
-                },
-                {
-                    "gunler":"Cumartesi-Pazar",
-                    "acilis":"10:00",
-                    "kapanis":"22:00",
-                    "kapali": false
-                }
-            ],
-            "yorumlar": [
-                {
-                    "yorumYapan":"Deniz YILDIZ",
-                    "puan":"4",
-                    "tarih":"29 Ekim 2022",
-                    "yorumMetni":"Kahveler iyi."
-                }
-            ]
-        }
-    });
+    axios
+        .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
+        .then(function  (response) {
+            detaySayfasiOlustur(res,response.data);
+        })
+        .catch(function (hata) {
+            hataGoster(res,hata);
+        });
+  
 };
 
 const yorumEkle = function(req, res, next) {
